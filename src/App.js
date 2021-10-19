fd5fe57bbce0e8914237c2db03a7350a55910e21
DmitriAleks <Dima.aleks.94@mail.ru>
2021-10-19 20:47:11 +0300

    added change theme

--- src/App.js
+++ src/App.js
@@ -4,7 +4,9 @@ import {Header} from "./component/Header";
 function App() {
     return (
         <div className="App">
-            <Header/>
+           <>
+           <Header/>
+           </>
         </div>
     );
 }
--- src/component/Header.jsx
+++ src/component/Header.jsx
@@ -1,25 +1,42 @@
 import styled from 'styled-components'
 import {Container} from "./Container";
-import {IoMoon,IoMoonOutline} from 'react-icons/io5'
+import {IoMoon, IoMoonOutline} from 'react-icons/io5'
+import {useState, useEffect} from 'react'
 
-const HeaderEl = styled.header``;
-const Wrapper = styled.div``;
+const HeaderEl = styled.header`
+  box-shadow: var(--shadow);
+  background-color: (--colors-ui-base);
+`;
+
+const Wrapper = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 2rem 0;
+`;
 const Title = styled.a.attrs({
     href: '/',
 })``;
 const ModeSwitcher = styled.div``;
 
 export const Header = () => {
-    return  ( <HeaderEl>
-<Container>
-    <Wrapper>
-        <Title>
-            Where is the world?
-            <ModeSwitcher>
-                <IoMoon/> Light theme
-            </ModeSwitcher>
-        </Title>
-    </Wrapper>
+    const [theme, setTheme] = useState('light');
+    useEffect(() => {
+        document.body.setAttribute('data-theme', theme)
+    }, [theme]);
+    const toggleTheme = () => {
+        setTheme(theme === 'light' ? 'dark' : 'light')
+    }
+    return (<HeaderEl>
+        <Container>
+            <Wrapper>
+                <Title>
+                    Where is the world? </Title>
+                <ModeSwitcher onClick={toggleTheme}>
+                    <IoMoon/> Light theme
+                </ModeSwitcher>
+
+            </Wrapper>
         </Container>
     </HeaderEl>)
 }
