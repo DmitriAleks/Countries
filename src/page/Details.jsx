764b5c1946c989bf928b75d1103f90c290fb146d
DmitriAleks <Dima.aleks.94@mail.ru>
2021-10-20 18:11:16 +0300

    create Detailis component and style

--- src/page/Details.jsx
+++ src/page/Details.jsx
@@ -1,10 +1,26 @@
-
-
-
+import axios from 'axios'
+import {IoArrowBack} from "react-icons/io5";
+import {useHistory, useParams} from 'react-router-dom'
+import {useEffect, useState} from "react";
+import {searchByCountry} from "../config";
+import {Button} from "../component/Button";
+import {Info} from "../component/Info";
 
 
 export const Details = ({match}) => {
-    return ( <div>
-        Details{match.params.name}
+    const {name} = useParams()
+    const {push, goBack} = useHistory()
+    const [country,setCountry] = useState(null)
+    useEffect(()=>{
+        axios.get(searchByCountry(name)).then(
+            ({data}) => setCountry(data[0])
+        );
+    },[name])
+    return (<div>
+        <Button onClick={goBack}><IoArrowBack/>Back</Button>
+        {country && (
+            <Info {...country}/>
+        )}
+
     </div>)
 }
